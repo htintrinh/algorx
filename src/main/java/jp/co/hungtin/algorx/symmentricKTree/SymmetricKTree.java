44b90ad8213058dac8fc45ef6281f42bc2e7fdd4
Tin Trinh <ts-tin.trinh@rakuten.com>
2020-09-06 18:21:56 +0900

    Finish check symmetric tree

--- src/main/java/jp/co/hungtin/algorx/symmentricKTree/SymmetricKTree.java
+++ src/main/java/jp/co/hungtin/algorx/symmentricKTree/SymmetricKTree.java
@@ -1,9 +1,12 @@
 package jp.co.hungtin.algorx.symmentricKTree;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -14,17 +17,17 @@ public class SymmetricKTree {
 
     primaryQueue.add(node);
     while (!primaryQueue.isEmpty() || !secondaryQueue.isEmpty()) {
-      traceNodeFromQueueToQueue(primaryQueue, secondaryQueue);
+      traceNodeFromQueueToQueue(primaryQueue, secondaryQueue, n -> System.out.printf("%d ", n.getValue()));
       System.out.println("");
-      traceNodeFromQueueToQueue(secondaryQueue, primaryQueue);
+      traceNodeFromQueueToQueue(secondaryQueue, primaryQueue, n -> System.out.printf("%d ", n.getValue()));
       System.out.println("");
     }
   }
 
-  public static void traceNodeFromQueueToQueue(Queue<Node> fromQueue, Queue<Node> toQueue) {
+  public static void traceNodeFromQueueToQueue(Queue<Node> fromQueue, Queue<Node> toQueue, Consumer<Node> action) {
     while (!fromQueue.isEmpty()) {
       var currentNode = fromQueue.poll();
-      System.out.printf("%d ", currentNode.getValue());
+      action.accept(currentNode);
       var children = currentNode.getChildren();
       if (children != null) {
         toQueue.addAll(children);
@@ -40,17 +43,52 @@ public class SymmetricKTree {
   }
 
   public static void main(String[] args) {
-    Node leftChild = Node.of(3, List.of(Node.of(9), Node.of(4), Node.of(1)));
-    Node rightChild = Node.of(3, List.of(Node.of(1), Node.of(4), Node.of(9)));
+    var leftChild = Node.of(3, List.of(Node.of(9), Node.of(4), Node.of(1)));
+    var rightChild = Node.of(3, List.of(Node.of(1), Node.of(4), Node.of(9)));
 
-    Node node = Node.of(1);
+    var node = Node.of(1);
     node.setChildren(List.of(leftChild, rightChild));
 
     printTree(node);
   }
 
+  public static boolean isSymmetricList(List<Integer> numbers) {
+    if (numbers.size() < 2) {
+      return true;
+    }
+
+    for (int i = 0, j = numbers.size() - 1; i <= j; i++, j--) {
+      if(numbers.get(i) != numbers.get(j)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
   // return if the tree node is symmetric or not
   public static boolean solution(Node node) {
+    if (node.getChildren() == null || node.getChildren().size() == 0) {
+      return true;
+    }
+    var primaryQueue = new LinkedList<Node>();
+    var secondaryQueue = new LinkedList<Node>();
+    primaryQueue.add(node);
+
+    while (!primaryQueue.isEmpty() || !secondaryQueue.isEmpty()) {
+      var childrenNodeValues = new ArrayList<Integer>();
+      traceNodeFromQueueToQueue(primaryQueue, secondaryQueue, n -> childrenNodeValues.add(n.value));
+      if (!isSymmetricList(childrenNodeValues)) {
+        return false;
+      }
+
+      var childrenNodeValues2 = new ArrayList<Integer>();
+      traceNodeFromQueueToQueue(secondaryQueue, primaryQueue, n -> childrenNodeValues2.add(n.value));
+      if (!isSymmetricList(childrenNodeValues2)) {
+        return false;
+      }
+    }
+
     return true;
   }
 
--- src/test/java/jp/co/hungtin/algorx/symmentricKTree/SymmetricKTreeTest.java
+++ src/test/java/jp/co/hungtin/algorx/symmentricKTree/SymmetricKTreeTest.java
@@ -1,14 +1,55 @@
 package jp.co.hungtin.algorx.symmentricKTree;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.List.of;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
 class SymmetricKTreeTest {
 
+  private static Stream<Arguments> providerForSymmetricKTree() {
+    var leftChild =
+        SymmetricKTree.Node.of(
+            3, of(SymmetricKTree.Node.of(9), SymmetricKTree.Node.of(4), SymmetricKTree.Node.of(1)));
+    var rightChild =
+        SymmetricKTree.Node.of(
+            3, of(SymmetricKTree.Node.of(1), SymmetricKTree.Node.of(4), SymmetricKTree.Node.of(9)));
 
-    @Test
-    void solutionShouldReturnExpectResult() {
+    var symmetricNode = SymmetricKTree.Node.of(1);
+    symmetricNode.setChildren(of(leftChild, rightChild));
 
-    }
+    return Stream.of(
+        Arguments.of(symmetricNode, true),
+        Arguments.of(SymmetricKTree.Node.of(1), true),
+        Arguments.of(
+            SymmetricKTree.Node.of(1, of(SymmetricKTree.Node.of(2), SymmetricKTree.Node.of(3))),
+            false));
+  }
+
+  private static Stream<Arguments> providerForIsSymmetricList() {
+    return Stream.of(
+        Arguments.of(List.of(1, 3, 4, 3, 1), true),
+        Arguments.of(List.of(1), true),
+        Arguments.of(List.of(1, 1), true),
+        Arguments.of(List.of(1, 2), false),
+        Arguments.of(List.of(1, 2, 3, 1, 2), false),
+        Arguments.of(List.of(1, 2, 3, 4, 5), false));
+  }
+
+  @ParameterizedTest
+  @MethodSource("providerForSymmetricKTree")
+  void solutionShouldReturnExpectResult(SymmetricKTree.Node tree, boolean expect) {
+    assertSame(expect, SymmetricKTree.solution(tree));
+  }
+
+  @ParameterizedTest
+  @MethodSource("providerForIsSymmetricList")
+  void isSymmetricList(List<Integer> numbers, boolean expect) {
+    assertSame(expect, SymmetricKTree.isSymmetricList(numbers));
+  }
 }
